67b7174eb91bc10dfa297e08ab47a13060be5abd
vishnurj99 <vishnurj99@gmail.com>
2022-08-31 12:02:44 +0530

    Updated

--- src/main/java/maven/application/Application.java
+++ src/main/java/maven/application/Application.java
@@ -1,6 +1,13 @@
 package maven.application;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+
+    public int countWords(String words){
+        String[] separateWords = StringUtils.split(words,' ');
+        return (separateWords == null) ? 0 : separateWords.length; 
+    }
     
     public Application() {
         System.out.println ("Inside Application");
@@ -10,5 +17,7 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
+        int count = app.countWords("Wow how many words are there in this sentence?");
+        System.out.println(count + "Words");
     }
 }
